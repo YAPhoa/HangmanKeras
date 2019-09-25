30d57f27dadeabc120ba8a7baaaef0750926857a
Yohanes Alfredo Phoa <yohanesa001@e.ntu.edu.sg>
2019-09-25 12:49:13 +0800

    first commit

---
+++ env.py
@@ -0,0 +1,116 @@
+import numpy as np
+
+class Hangman(object) :
+    """ Hangman Game.
+    Implemented Hangman game with almost gym-like environment
+    Requires word source(word_src) to build dictionary
+    Reward parameter can be set manually.
+    Game board will refers to the word that we are asked to guess.
+
+    Brief Description :
+    reset : starts the game, cleared guessed letters memory, 
+            select random word from word collection,
+            returns a masked version of the word e.g. 'alpaca' = '------'
+
+    step : accepts an alphabet as input, 
+           Evaluation of this input will occur in these steps :
+           1. Check if the input is used already, if used return similar state, 
+              if there is repeated_guessing_penalty, will return this penalty as reward.
+              If not used go to next step.
+
+           2. if the letter exists in the game board,
+              will reveal every existence of the letter in the gameboard.
+              if not the last letter of the problem will return correct_reward,
+              otherwise return win_reward as reward
+
+            3. If the letter does not exist in the game board. Return similar game board.
+               Will reduce live by 1. If lives reaches 0. Game over.
+               Return false_reward if not game over, else return lose_reward 
+    """
+    def __init__(self , 
+                 word_src, 
+                 max_lives = 6 , 
+                 win_reward = 30,
+                 correct_reward = 1,
+                 repeated_guessing_penalty = -100,
+                 lose_reward = -0, 
+                 false_reward = -0,
+                 verbose = False) :
+        if type(word_src) == list :
+            self.words = word_src
+        else :
+            with open(word_src, 'r') as f :
+                self.words = f.read().splitlines()
+        self.max_lives = max_lives
+        self.win_reward = win_reward
+        self.correct_reward = correct_reward
+        self.lose_reward = lose_reward
+        self.false_reward = false_reward
+        self.verbose = verbose
+        self.repeated_guessing_penalty = repeated_guessing_penalty
+        
+    def pick_random(self) :
+        self.guess_word = np.random.choice(self.words)
+        
+    def reset(self) :
+        self.curr_live = self.max_lives
+        self.pick_random()
+        self.guessing_board = ['-' for i in range(len(self.guess_word))]
+        self.correct_guess = 0
+        self.guessed = []
+        self.done = False
+        if self.verbose :
+            print('Game Starting')
+            print('Current live :', self.curr_live)
+        return self.show_gameboard()
+        
+
+    def show_gameboard(self) :
+        board = ''.join(self.guessing_board)
+        if self.verbose:
+            print(board)
+            print()
+        return board
+        
+    def step(self, letter) :
+        if not(letter.isalpha()) :
+            raise TypeError('Can only accept alphabet')
+        letter = letter.lower()
+
+        if letter not in self.guessed :
+            self.guessed.append(letter)
+        else :
+            if self.verbose :
+                print('Word used already')
+            return self.show_gameboard(), self.repeated_guessing_penalty, self.done, {}
+
+
+        if letter in self.guess_word :
+            for i in range(len(self.guess_word)) :
+                if letter == self.guess_word[i] :
+                    self.guessing_board[i] = letter
+                    self.correct_guess += 1
+            if self.correct_guess == len(self.guess_word) :
+                self.done = True
+                if self.verbose :
+                    print('You Win')
+                    print('Word is', self.guess_word)
+                return self.guess_word, self.win_reward, self.done, {'ans' : self.guess_word}
+            else :                
+                return self.show_gameboard(), self.correct_reward, self.done, {}
+        else :
+            self.curr_live -= 1
+            if self.curr_live == 0 :
+                self.done = True
+                if self.verbose :
+                    print('You Lose')
+                    print('Word is', self.guess_word)
+                return self.show_gameboard(), self.lose_reward, self.done, {'ans' : self.guess_word}
+            else :
+                if self.verbose :
+                    print('Current live :', self.curr_live)
+                return self.show_gameboard(), self.false_reward, self.done, {}
+
+
+if __name__=='__main__' :
+    pass
---
+++ train.py
@@ -0,0 +1,122 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = '-1'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+from tqdm import tqdm
+
+from env import Hangman
+from network import Network, Agent
+from utils import *
+from keras import optimizers
+
+import numpy as np
+import random
+
+maxlen = 29
+
+word_src = 'words_250000_train.txt'
+
+policy_net = Network()
+player = Agent(policy_net)
+policy_net.summary()
+
+
+save_episode = 5000
+view_episode = 500
+update_episode = 3
+avg_correct = 0
+wins_avg = 0
+n_trials = 20000
+progbar = tqdm(range(n_trials))
+
+""" 
+Warmup Phase
+Since the network can only collect data by interacting with the environment, 
+to collect more data max_lives parameter or the maximum number of trial before game over is set 
+slightly extra than the actual game parameter so episodes are longer. 
+"""
+
+game_params = {'max_lives' : 8}
+env = Hangman(word_src, **game_params)
+
+#player.model.load_weights('policy.h5')
+
+print('Training Start ...', end = '\n\n')
+
+for episode_set in progbar :
+    for _ in range(update_episode) :
+        total_reward = 0    
+        state = env.reset()
+        done = False
+        correct_count = 0
+        while not done :
+            guess = player.select_action(state)
+            state, reward, done, ans = env.step(guess)
+            if reward > 0 :
+                correct_count += 1.0
+            if reward == env.win_reward :
+                wins_avg += 1.0
+        player.finalize_episode(ans['ans'])
+        avg_correct += correct_count
+    loss = player.train_model()
+    progbar.set_description("Loss : {:.3f}              ".format(loss))
+
+    if (episode_set +1) % view_episode == 0 :
+        views = (episode_set + 1,avg_correct/(update_episode*view_episode), view_episode, wins_avg/(update_episode*view_episode))
+        print('Episode {} -------- Average Correct Count : {:.3f}     Last {} winrate : {:.3f}'.format(*views))
+        if loss is not None :
+            print('Loss :', loss)
+            print()
+            avg_correct = 0
+            wins_avg = 0
+
+    if (episode_set +1) % save_episode == 0 :
+        player.model.save('policy.h5', include_optimizer=False)
+
+print()
+game_params = {'max_lives' : 6}
+env = Hangman(word_src, **game_params)
+
+""" 
+Final Phase, 
+Collect data by interacting with similar parameter as the actual game.
+"""
+
+n_trials = 70000
+progbar = tqdm(range(n_trials))
+
+player.model.compile(optimizer= optimizers.Adam(3e-5, clipnorm=1))
+
+for episode_set in progbar :
+    for _ in range(update_episode) :
+        total_reward = 0    
+        state = env.reset()
+        done = False
+        correct_count = 0
+        while not done :
+            guess = player.select_action(state)
+            state, reward, done, ans = env.step(guess)
+            #player.append_sample(letter_dict[guess] - 1, reward)
+            if reward > 0 :
+                correct_count += 1.0
+            if reward == env.win_reward :
+                wins_avg += 1.0
+        player.finalize_episode(ans['ans'])
+        avg_correct += correct_count
+    loss = player.train_model()
+    progbar.set_description("Loss : {:.3f}              ".format(loss))
+
+    if (episode_set +1) % view_episode == 0 :
+        views = (episode_set + 1,avg_correct/(update_episode*view_episode), view_episode, wins_avg/(update_episode*view_episode))
+        print('Episode {} -------- Average Correct Count : {:.3f}     Last {} winrate : {:.3f}'.format(*views))
+        if loss is not None :
+            print('Loss :', loss)
+            print()
+            avg_correct = 0
+            wins_avg = 0
+
+    if (episode_set +1) % save_episode == 0 :
+        player.model.save('policy.h5', include_optimizer=False)
+
+
